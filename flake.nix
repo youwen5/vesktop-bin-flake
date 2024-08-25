14f82df3726f8b20ec2e7753ae077b0e9033d916
Youwen Wu <youwenw@gmail.com>
2024-08-25 06:03:14 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Alternative Vesktop package that fetches the official binary instead of building from source.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    outputs = flake-utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = import ./package.nix {inherit system pkgs;};
+    });
+  in
+    outputs
+    // {
+      overlays.default = final: prev: {
+        vesktop = outputs.${prev.system}.packages.vesktop;
+      };
+    };
+}
---
+++ package.nix
@@ -0,0 +1,60 @@
+{
+  pkgs,
+  system,
+}: {
+  default = with pkgs; let
+    pname = "vesktop";
+    version = "1.5.3";
+
+    # Format is like:
+    # `Vesktop-1.5.3-arm64.AppImage`
+    # or `Vesktop-1.5.3.AppImage`
+
+    src = let
+      cpuQualifier =
+        if system == "aarch64-linux"
+        then "-arm64"
+        else "";
+      sha256Hash =
+        if system == "aarch64-linux"
+        then "1pvgjfbiw607z2v1cyzl16dp79myz165d1hpv8cq3rywd53nhpg0"
+        else "0rcwszcnb3ir2j87fr4ibg3x6id5s7l9hh7565m732fmaaf6qlks";
+    in
+      fetchurl {
+        url = "https://github.com/Vencord/Vesktop/releases/download/v${version}/${pname}-${version}${cpuQualifier}.AppImage";
+        sha256 = sha256Hash;
+      };
+
+    appimageContents = appimageTools.extract {
+      inherit pname version src;
+    };
+
+    desktopItem = makeDesktopItem {
+      name = pname;
+      desktopName = "Vesktop";
+      exec = "vesktop %U --ozone-platform-hint=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime";
+      icon = "vesktop";
+      startupWMClass = "Vesktop";
+      genericName = "Internet Messenger";
+      keywords = [
+        "discord"
+        "chat"
+      ];
+      categories = [
+        "Network"
+        "InstantMessaging"
+        "Chat"
+      ];
+    };
+  in
+    appimageTools.wrapType2 {
+      inherit pname version src;
+
+      extraInstallCommands = ''
+        mkdir -p $out/share
+        cp -rt $out/share ${desktopItem}/share/applications ${appimageContents}/usr/share/icons
+        chmod -R +w $out/share
+        mv $out/share/icons/hicolor/{16x16,256x256}
+      '';
+    };
+}
