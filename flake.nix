70a9d9a16dad7b97afc1cf08b7d0e04751dd3fda
Youwen Wu <youwenw@gmail.com>
2024-12-10 20:32:12 -0800

    feat: update to 1.5.4 and overhaul flake to wrap binary

--- flake.nix
+++ flake.nix
@@ -3,25 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    vesktop-bin-aarch64 = {
+      url = "https://github.com/Vencord/Vesktop/releases/download/v1.5.4/vesktop-1.5.4-arm64.tar.gz";
+      flake = false;
+    };
+    vesktop-bin-x86_64 = {
+      url = "https://github.com/Vencord/Vesktop/releases/download/v1.5.4/vesktop-1.5.4.tar.gz";
+      flake = false;
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }: let
-    systems = ["x86_64-linux" "aarch64-linux"];
-    outputs = flake-utils.lib.eachSystem systems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages = import ./package.nix {inherit system pkgs;};
-    });
-  in
-    outputs
-    // {
-      overlays.default = final: prev: {
-        vesktop = outputs.packages.${prev.system}.vesktop;
+  outputs =
+    inputs@{
+      flake-parts,
+      self,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          packages.default = pkgs.callPackage ./vesktop.nix {
+            version = "1.5.4";
+            src = if system == "aarch64-linux" then inputs.vesktop-bin-aarch64 else inputs.vesktop-bin-x86_64;
+          };
+        };
+      flake = {
+        overlays.default = final: prev: {
+          vesktop = self.packages.${prev.system}.default;
+        };
       };
     };
 }
---
+++ vesktop.nix
@@ -0,0 +1,82 @@
+{
+  stdenv,
+  src,
+  version,
+  autoPatchelfHook,
+  mesa,
+  libdrm,
+  expat,
+  libxkbcommon,
+  systemd,
+  alsa-lib,
+  at-spi2-atk,
+  libgcc,
+  nss,
+  pango,
+  gtk3,
+  nspr,
+  cairo,
+  cups,
+  libGL,
+  makeShellWrapper,
+  wrapGAppsHook3,
+  pipewire,
+  libpulseaudio,
+  xorg,
+}:
+stdenv.mkDerivation {
+  inherit src version;
+
+  pname = "vesktop";
+
+  nativeBuildInputs =
+    [
+      autoPatchelfHook
+      makeShellWrapper
+      wrapGAppsHook3
+      mesa
+      libdrm
+      expat
+      libxkbcommon
+      systemd
+      alsa-lib
+      at-spi2-atk
+      libgcc
+      nss
+      pango
+      gtk3
+      nspr
+      cairo
+      libGL
+      pipewire
+      libpulseaudio
+    ]
+    ++ (with xorg; [
+      libxcb
+      libXext
+      libX11
+      libXrandr
+      libXdamage
+      libXfixes
+      libXcomposite
+      cups
+    ]);
+
+  dontWrapGApps = true;
+
+  appendRunpaths = [
+    "${libGL}/lib"
+    "${pipewire}/lib"
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin $out/lib
+    cp -r * $out/lib
+    wrapProgramShell $out/lib/vesktop \
+         "''${gappsWrapperArgs[@]}" \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
+        --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}/"
+
+    ln -s $out/lib/vesktop $out/bin/vesktop
+  '';
+}
